9db6a801352f43068ea008ad6c8d6dc0e6b4b342
siemenHerremans <siemen_herremans@hotmail.com>
2020-10-06 14:58:47 +0200

    Fix ADC (not tested)

--- CPS-Ender/ADC.c
+++ CPS-Ender/ADC.c
@@ -13,7 +13,8 @@
 void ADC_init(){
 	
 	//Reference: analog ground, input: ADC 13, left adjusted
-	ADMUX = 0b101;	
+	ADMUX |= (1 << REFS1) | (1 << REFS0);
+	ADMUX |= 0b101;
 	//ADC 13 input (MSB)
 	ADCSRB = 1 << 3;
 	//Enable and factor 128 prescaler (16MHz -> 128 kHz)
@@ -25,7 +26,7 @@ int16_t ADC_read(){
 	//Start conversion
 	ADCSRA |= 1 << 6;
 	//Wait for result
-	while ( (ADCSRA & (1 << 4)) == 0);
+	while ( (ADCSRA  & (1 << ADSC)) == 1);
 	//Read results
 	int16_t result = ADCL + (ADCH << 8);
 	//Clear "finished"-bit
